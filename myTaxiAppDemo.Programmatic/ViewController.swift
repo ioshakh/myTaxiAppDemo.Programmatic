cdb88eac68c775d5f51f034d38c0387644fdbf6f
Shakhzod <79221577+ioshakh@users.noreply.github.com>
2021-12-24 21:49:10 +0500

    Initial Commit

---
+++ myTaxiAppDemo.Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTaxiAppDemo.Programmatic
+//
+//  Created by Shakhzod Bektemirov on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
